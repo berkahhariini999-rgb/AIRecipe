c71462c22fd357b1276a00e3a82ddcf6a61f238b
berkahhariini999-rgb <berkahhariini999@gmail.com>
2026-06-14 16:07:29 +0700

    Initial Commit

---
+++ AIRecipe/AIRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIRecipeApp.swift
+//  AIRecipe
+//
+//  Created by Iqbal Alhadad on 14/06/26.
+//
+
+import SwiftUI
+
+@main
+struct AIRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIRecipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIRecipe
+//
+//  Created by Iqbal Alhadad on 14/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
